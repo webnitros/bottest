25493719c8f17e168e5ebed3b38b8219a1b1c0f9
Андрей <info@bustep.ru>
2022-07-24 17:57:30 +0300

    autocommit

--- src/Helpers/Funcompare.php
+++ src/Helpers/Funcompare.php
@@ -40,8 +40,18 @@ class Funcompare
                 } else {
                     $foundKey = array_search($oldArr[$tmpOldIndex], $newArr, true);
                     if ($foundKey != '' && $foundKey > $tmpNewIndex) {
+
+
+                        $lastNew = $resArr;
+
+                        $last = array_pop($lastNew);
+                        // Если равно
+                        if ($last === '<old:{%}>') {
+                            array_pop($resArr);
+                        }
                         for ($p = $tmpNewIndex; $p < $foundKey; $p++) {
-                            array_push($resArr, sprintf('<%s:%s>', $this->_newLabel, $newArr[$p]));
+                            array_push($resArr, $newArr[$p]);
+                            #array_push($resArr, sprintf('<%s:%s>', $this->_newLabel, $newArr[$p]));
                         }
                         array_push($resArr, $oldArr[$tmpOldIndex]);
                         $tmpOldIndex++;
@@ -61,6 +71,42 @@ class Funcompare
             $textFinal .= $val . ' ';
         }
         return $textFinal;
+    }
+
+    public function compareArray($oldArr, $newArr)
+    {
+        $result = array();
+
+        $keys = array_unique(array_merge(array_keys((array)$oldArr), array_keys($newArr)));
+        foreach ($keys as $k) {
+            if (isset($oldArr[$k]) && isset($newArr[$k])) {
+                if (is_array($oldArr[$k]) && is_array($newArr[$k])) {
+                    $tmp = $this->compareArray($oldArr[$k], $newArr[$k]);
+                    if ($tmp) {
+                        $result[$k] = $tmp;
+                    }
+                } elseif (is_array($oldArr[$k]) || is_array($newArr[$k]) || $oldArr[$k] != $newArr[$k]) {
+                    $result[$k][$this->_oldLabel] = $oldArr[$k];
+                    $result[$k][$this->_newLabel] = $newArr[$k];
+                }
+            } elseif (isset($oldArr[$k]) && !isset($newArr[$k])) {
+                $result[$k][$this->_oldLabel] = $oldArr[$k];
+                $result[$k][$this->_newLabel] = null;
+            } elseif (!isset($oldArr[$k]) && isset($newArr[$k])) {
+                $result[$k][$this->_oldLabel] = null;
+                $result[$k][$this->_newLabel] = $newArr[$k];
+            }
+        }
+
+        return $result;
+    }
+
+    public function compareJson($oldJson, $newJson)
+    {
+        $oldArr = json_decode($oldJson, true);
+        $newArr = json_decode($newJson, true);
+        $res = $this->compareArray($oldArr, $newArr);
+        return json_encode($res, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
     }
 
 }
